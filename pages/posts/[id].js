d7bfaa25d3a1400bb783e2cd895b7eab00cd8441
showthesunli <showthesunli@gmail.com>
2022-04-10 23:30:18 +0800

    dynamic routes page7 done

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,20 +1,21 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/post'
 import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
     return (<Layout>
         <Head>
             <title>{postData.title}</title>
         </Head>
-
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-        <br />
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <article>
+            <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+                <Date dateString={postData.date}/>
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
     </Layout>)
 }
 
