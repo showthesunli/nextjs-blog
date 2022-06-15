ead088ba4bc011b63790ced6cb64e70db0774c1b
showthesunli <showthesunli@gmail.com>
2022-06-15 10:22:36 +0800

    fix some dateTime issue

--- pages/mongo.js
+++ pages/mongo.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '@/components/layout'
 import utilStyles from '@/styles/utils.module.css'
 import Link from 'next/link'
-import Date from '@/components/date'
 import {getArticleList} from '@/lib/db-manager/repository/db_tools'
 
 export async function getStaticProps() {
@@ -39,7 +38,7 @@ export default function Home({ allPostsData }) {
                 </Link>
                 <br/>
                 <small className={utilStyles.lightText}>
-                  {doc_date/* <Date dateString={doc_date}/> */}
+                  {doc_date}
                 </small>
               </li>
               ))
